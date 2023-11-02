3965b4b7d0c800fac8b6a2398d5f011da0fe2cd5
HasnaAkter <hasnaakter.cse@gmail.com>
2023-11-03 04:43:55 +0600

    drag&drop

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,3 +1,8 @@
+import { useState, useRef } from "react";
+import { useDrag, useDrop } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd";
+
 import img1 from "../assets/image-1.webp";
 import img2 from "../assets/image-2.webp";
 import img3 from "../assets/image-3.webp";
@@ -10,8 +15,57 @@ import img9 from "../assets/image-9.webp";
 import img10 from "../assets/image-10.jpeg";
 import img11 from "../assets/image-11.jpeg";
 import img12 from "../assets/img12.jpg";
-import { useState } from "react";
 import "../pages/Css/Grid.css";
+
+const DraggableImage = ({
+  image,
+  index,
+  moveImage,
+  isSelected,
+  toggleSelection,
+}) => {
+  const ref = useRef(null);
+  const [, drop] = useDrop({
+    accept: "IMAGE",
+    hover(draggedItem) {
+      if (draggedItem.index !== index) {
+        moveImage(draggedItem.index, index);
+        draggedItem.index = index;
+      }
+    },
+  });
+  const [{ isDragging }, drag] = useDrag({
+    type: "IMAGE",
+    item: { index },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  drag(drop(ref));
+
+  return (
+    <div
+      ref={ref}
+      className={`item ${isDragging ? "dragging" : ""} ${
+        index === 0 ? "row-span-2 col-span-2" : ""
+      }`}
+    >
+      <div className="item">
+        <label>
+          <input
+            type="checkbox"
+            checked={isSelected}
+            onChange={() => toggleSelection(index)}
+          />
+          <img src={image} alt={`Image ${index + 1}`} />
+        </label>
+      </div>
+      
+    </div>
+    
+  );
+};
 
 const Form = () => {
   const [images, setImages] = useState([
@@ -30,9 +84,16 @@ const Form = () => {
 
   const [selectedItems, setSelectedItems] = useState([]);
 
+  const moveImage = (fromIndex, toIndex) => {
+    const updatedImages = [...images];
+    const [movedImage] = updatedImages.splice(fromIndex, 1);
+    updatedImages.splice(toIndex, 0, movedImage);
+    setImages(updatedImages);
+  };
+
   const toggleSelection = (index) => {
-    const isSelected = selectedItems.includes(index);
     setSelectedItems((prevSelected) => {
+      const isSelected = prevSelected.includes(index);
       if (isSelected) {
         return prevSelected.filter((selected) => selected !== index);
       } else {
@@ -51,56 +112,44 @@ const Form = () => {
       (_, index) => !selectedItems.includes(index)
     );
     setImages(remainingImages);
-
-    console.log("Deleting selected items:", selectedItems);
-
     setSelectedItems([]);
   };
 
   return (
-    <div className="my-5 border-solid border-2 bg-gray-100 shadow-lg shadow-gray-500/50">
-      <div className="flex">
-        <div className="p-5">
-          <h2 className="font-bold text-2xl">
-            {selectedItems.length > 0
-              ? `Selected items: ${selectedItems.length}`
-              : "Gallery"}
-          </h2>
-        </div>
-        {selectedItems.length > 0 && (
-          <div className="ml-auto p-5">
-            <button onClick={handleDelete}>
-              Delete ({selectedItems.length})
-            </button>
+    <DndProvider backend={HTML5Backend}>
+      <div className="my-5 border-solid border-2 bg-gray-100 shadow-lg shadow-gray-500/50">
+        <div className="flex">
+          <div className="p-5">
+            <h2 className="font-bold text-2xl">
+              {selectedItems.length > 0
+                ? `Selected items: ${selectedItems.length}`
+                : "Gallery"}
+            </h2>
           </div>
-        )}
-      </div>
-      <hr className="border-solid bg-gray-300" />
-      <div className="grid grid-cols-5 gap-4 p-10">
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className={`item ${
-              selectedItems.includes(index) ? "deleted" : ""
-            } ${index === 0 ? "row-span-2 col-span-2" : ""}`}
-          >
-            <div className="item">
-              <label>
-                <input
-                  type="checkbox"
-                  checked={selectedItems.includes(index)}
-                  onChange={() => toggleSelection(index)}
-                />
-                <img src={image} alt={`Image ${index + 1}`} />
-              </label>
+          {selectedItems.length > 0 && (
+            <div className="ml-auto p-5">
+              <button onClick={handleDelete}>Delete</button>
             </div>
-          </div>
-        ))}
-        <div className="item-added-images">
+          )}
+        </div>
+        <hr className="border-solid bg-gray-300" />
+        <div className="grid grid-cols-5 gap-4 p-10">
+          {images.map((image, index) => (
+            <DraggableImage
+              key={index}
+              index={index}
+              image={image}
+              moveImage={moveImage}
+              isSelected={selectedItems.includes(index)}
+              toggleSelection={toggleSelection}
+            />
+          ))}
+          <div className="item-added-images">
           <img src={img12} alt="" />
         </div>
+        </div>
       </div>
-    </div>
+    </DndProvider>
   );
 };
 
